b499f2b65e742a5faeeab6dfa91236e496f70fe8
MrSAHID <54776910+shahid0980@users.noreply.github.com>
2026-02-22 04:14:30 +0530

    Update Header.tsx
    add shadow on navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   }
   return (
     <header className=" sticky top-0 z-50">
-      <nav className="bg-white dark:bg-gray-950 w-full  font-semibold  transition-colors duration-300">
+      <nav className="bg-white dark:bg-gray-950 w-full  font-semibold  transition-colors duration-300 shadow-lg">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-2">
             {/* bg-linear-to-r from-blue-100 to-blue-400 text-transparent bg-clip-text */}
